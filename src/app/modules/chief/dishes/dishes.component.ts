a82adc1b0531dbb12eb5e788222483b1d6d25612
AmmarElredeny <ammar.foad@afaqy.com>
2020-11-22 23:37:21 +0200

    fix page changed error in dishes list

--- src/app/modules/chief/dishes/dishes.component.ts
+++ src/app/modules/chief/dishes/dishes.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { DishService } from 'src/app/services/chief/dish.service';
 import { UserService } from 'src/app/services/user/user.service';
 import { AddDishComponent } from './add-dish/add-dish.component';
@@ -8,13 +8,15 @@ import { LanguageService } from 'src/app/services/language.service';
 import { ToastrService } from 'ngx-toastr';
 import { WarningComponent } from 'src/app/sharedModules/warning/warning.component';
 import { environment } from 'src/environments/environment';
+import { Subject } from 'rxjs';
+import { distinctUntilChanged, take, takeWhile } from 'rxjs/operators';
 
 @Component({
   selector: 'app-dishes',
   templateUrl: './dishes.component.html',
   styleUrls: ['./dishes.component.css']
 })
-export class DishesComponent implements OnInit {
+export class DishesComponent implements OnInit,OnDestroy {
   filter: Filter = {  ChiefID: this.userService.currentUser.chief_id, SearchField:'',per_page:10,current_page: 1};
   dishes: any = [];
   myUrl: any;
@@ -22,6 +24,8 @@ export class DishesComponent implements OnInit {
   loading = false;
   addEditaddressModel: BsModalRef;
   warningModel: BsModalRef;
+  subject:Subject<any> = new Subject();
+  alive:boolean=true;
   constructor(private dishService: DishService,
               private userService: UserService,
               private kitchenService: KitchenService,
@@ -32,13 +36,19 @@ export class DishesComponent implements OnInit {
 
   ngOnInit(){
     this.myUrl = environment.api_imges;
-    this.userService.GetByID(this.userService.currentUser.id).subscribe(res => {this.appprove = res.Data.is_approved; });
+    this.userService.GetByID(this.userService.currentUser.id).pipe(takeWhile(()=>this.alive)).subscribe(res => {this.appprove = res.Data.is_approved; });
     this.getDishes();
+
+    this.subject.pipe(distinctUntilChanged(),takeWhile(()=>this.alive)).subscribe(res=>{
+      console.log(res);
+      this.filter.current_page =res;
+      this.getDishes();
+    })
   }
 
   getDishes() {
     this.loading = true;
-    this.kitchenService.GetListpaging(this.filter).subscribe(
+    this.kitchenService.GetListpaging(this.filter).pipe(takeWhile(()=>this.alive)).subscribe(
       res => {
         this.loading = false;
         this.dishes = res.Data;
@@ -50,7 +60,7 @@ export class DishesComponent implements OnInit {
     this.warningModel.content.boxObj.msg = 'Are you sure you want to delete this Dish ?';
     this.warningModel.content.onClose = (cancel) => {
       if (cancel) {
-        this.kitchenService.DeleteDish(DishID).subscribe(res => {
+        this.kitchenService.DeleteDish(DishID).pipe(takeWhile(()=>this.alive)).subscribe(res => {
             if (res.Success) {
               this.dishes = res.Data;
               this.toastr.success(res.Message);
@@ -70,7 +80,9 @@ export class DishesComponent implements OnInit {
   }
   
   pageChanged(event: any): void {
-    this.filter.current_page = event.page;
-    this.getDishes();
+    this.subject.next(event.page);
+   }
+   ngOnDestroy(){
+     this.alive=false;
    }
 }
